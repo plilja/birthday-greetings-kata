70a34c3eacd4a26a0ddccdb5b77c12b549b6f775
Patrik Lilja <lilja.patrik@gmail.com>
2019-01-03 11:01:58 +0100

    Remove equals and hashCode implementations

--- src/test/java/it/xpug/kata/birthdaygreetings/EmployeeTest.java
+++ src/test/java/it/xpug/kata/birthdaygreetings/EmployeeTest.java
@@ -1,9 +1,11 @@
 package it.xpug.kata.birthdaygreetings;
-import static org.junit.Assert.*;
 
-import org.junit.*;
+import org.junit.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class EmployeeTest {
@@ -15,15 +17,4 @@ public class EmployeeTest {
 		assertTrue("his birthday", employee.isBirthday(LocalDate.parse("2008-01-31")));
 	}
 
-	@Test
-	public void equality() {
-		Employee base = new Employee("First", "Last", "1999-09-01", "first@last.com");
-		Employee same = new Employee("First", "Last", "1999-09-01", "first@last.com");
-		Employee different = new Employee("First", "Last", "1999-09-01", "boom@boom.com");
-
-		assertFalse(base.equals(null));
-		assertFalse(base.equals(""));
-		assertTrue(base.equals(same));
-		assertFalse(base.equals(different));
-	}
 }
